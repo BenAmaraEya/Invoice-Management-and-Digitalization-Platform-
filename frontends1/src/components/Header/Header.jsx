45d1ae2150d0fd01c3ad7455d2761d80b7f18f05
wafa hamad <hamedw594@gmail.com>
2024-04-08 00:40:30 +0200

    s3

--- frontends1/src/components/Header/Header.jsx
+++ frontends1/src/components/Header/Header.jsx
@@ -1,44 +1,34 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faSignOutAlt, faCog, faBell } from "@fortawesome/free-solid-svg-icons";
+import { faCog, faBell } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import logo from '../../assets/images/TTlogo.png';
 import io from 'socket.io-client';
-import axios from 'axios';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import NotificationBadge, { Effect } from 'react-notification-badge';
+
 const Header = () => {
   const navigate = useNavigate();
   const userId = localStorage.getItem("userId");
-  const [isBordereauMenuOpen, setIsBordereauMenuOpen] = useState(false);
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
   const [isNotificationMenuOpen, setIsNotificationMenuOpen] = useState(false);
   const [notifications, setNotifications] = useState([]);
 
-  const handleToggleBordereauMenu = () => {
-    setIsBordereauMenuOpen(!isBordereauMenuOpen);
-  };
-
   const handleToggleUserMenu = () => {
     setIsUserMenuOpen(!isUserMenuOpen);
-  };
-
-  const handleNatureSelection = (nature) => {
-    
-    navigate('/uploadFacture/'+nature, { state: { nature } });
-    console.log(nature);
-    setIsBordereauMenuOpen(false); // Close the menu after selecting a nature
   };
 
   const handleUpdatePassword = () => {
     navigate(`/updatePass/${userId}`);
     setIsUserMenuOpen(false); // Close the menu after selecting an option
   };
-  const handleReclamation =()=>{
-    navigate(`/reclamation/${userId}`)
-  }
+
+  const handleReclamation = () => {
+    navigate(`/reclamation/${userId}`);
+    setIsUserMenuOpen(false); // Close the menu after selecting an option
+  };
 
   const handleLogout = async () => {
     try {
@@ -60,19 +50,17 @@ const Header = () => {
     } catch (error) {
       console.error("Error during logout:", error);
     }
-    setIsUserMenuOpen(false); 
+    setIsUserMenuOpen(false);
   };
+
   useEffect(() => {
-    
     const storedNotifications = JSON.parse(localStorage.getItem("notifications")) || [];
     setNotifications(storedNotifications);
     const socket = io('http://localhost:3006');
-    socket.on('newStatuts', async (statut,num) => {
+    socket.on('newStatuts', async (statut, num) => {
       try {
-        
-        // Add new notification to the array
         const newNotification = {
-          message: {statut,num}
+          message: { statut, num }
         };
         setNotifications(prevNotifications => {
           const updatedNotifications = [...prevNotifications, newNotification];
@@ -88,19 +76,23 @@ const Header = () => {
       socket.disconnect();
     };
   }, []);
+
   const handleToggleNotificationMenu = () => {
     setIsNotificationMenuOpen(!isNotificationMenuOpen);
   };
+
   return (
     <header className="header">
-       <div className="logo-container">
-    <img src={logo} alt="" className="logo" />
-  </div>
+      <div className="logo-container">
+        <img src={logo} alt="" className="logo" />
+      </div>
       <div className="navigation">
         <ul className="menu d-flex align-items-center gap-5">
-          
-         
-          
+          <li className="nav__item">
+            <NavLink to="/home" activeClassName="active" exact>
+              Accueil
+            </NavLink>
+          </li>
           <li className="nav__item">
             <NavLink to={`/dashboard/${userId}`} activeClassName="active" exact>
               Dashboard
@@ -113,28 +105,9 @@ const Header = () => {
           </li>
           <li className="nav__item">
             <NavLink to={`/listreclamation/${userId}`} activeClassName="active" exact>
-             Réclamations
+              Réclamations
             </NavLink>
           </li>
-         
-         { /*<li className="nav__item bordereau-item">
-            <div className="dropdown">
-              <div className="dropdown-btn" onClick={handleToggleBordereauMenu}>
-                Bordereau
-              </div>
-              {isBordereauMenuOpen && (
-                <div className="dropdown-content">
-                  <button className="dropdown-item" onClick={() => handleNatureSelection('3WMTND')}>
-                    TND
-                  </button>
-                  <button className="dropdown-item" onClick={() => handleNatureSelection('Nature2')}>
-                    Nature 2
-                  </button>
-                </div>
-              )}
-            </div>
-          </li>*/}
-        
           <li className="nav__item user-settings-item">
             <div className="dropdown">
               <div className="dropdown-btn" onClick={handleToggleUserMenu}>
@@ -142,7 +115,7 @@ const Header = () => {
               </div>
               {isUserMenuOpen && (
                 <div className="dropdown-content">
-                   <button className="dropdown-item" onClick={handleReclamation}>
+                  <button className="dropdown-item" onClick={handleReclamation}>
                     Réclamations
                   </button>
                   <button className="dropdown-item" onClick={handleUpdatePassword}>
@@ -151,7 +124,6 @@ const Header = () => {
                   <button className="dropdown-item" onClick={handleLogout}>
                     Logout
                   </button>
-
                 </div>
               )}
             </div>
@@ -159,40 +131,35 @@ const Header = () => {
           <li className="nav__item">
             <div className="dropdown">
               <div className="dropdown-btn" onClick={handleToggleNotificationMenu}>
-                
-              <FontAwesomeIcon icon={faBell} className="notif-icon" />
-              {notifications.length > 0 && (
-      <div className="notification-badge-container">
-      <NotificationBadge count={notifications.length} effect={Effect.SCALE} />
-    </div>
-    )}
-              
+                <FontAwesomeIcon icon={faBell} className="notif-icon" />
+                {notifications.length > 0 && (
+                  <div className="notification-badge-container">
+                    <NotificationBadge count={notifications.length} effect={Effect.SCALE} />
+                  </div>
+                )}
               </div>
               {isNotificationMenuOpen && (
-  <div className="dropdown-content">
-    {notifications.length === 0 ? (
-      <div>Pas de nouveau notification</div>
-    ) : (
-      notifications.map(notification => (
-        <div key={notification.id} onClick={() => {
-         
-          NotificationManager.info(notification.message.statuts);
-          setNotifications(notifications.filter(n => n.id !== notification.id));
-          localStorage.setItem("notifications", JSON.stringify(notifications.filter(n => n.id !== notification.id)));
-        }} className="notification-item">
-       {notification.message && notification.message.statuts && notification.message.statuts.num && (
-      <span>N°:{notification.message.statuts.num} - {notification.message.statuts.statuts}</span>
-    )}
-        </div>
-      ))
-    )}
-  </div>
-)}
-</div>
+                <div className="dropdown-content">
+                  {notifications.length === 0 ? (
+                    <div>Pas de nouveau notification</div>
+                  ) : (
+                    notifications.map((notification, index) => (
+                      <div key={index} onClick={() => {
+                        NotificationManager.info(notification.message.statut.statuts);
+                        setNotifications(notifications.filter(n => n.id !== index));
+                        localStorage.setItem("notifications", JSON.stringify(notifications.filter(n => n.id !== index)));
+                      }} className="notification-item">
+                        N°: {notification.message.statut.num} - {notification.message.statut.statuts}
+                      </div>
+                    ))
+                  )}
+                </div>
+              )}
+            </div>
           </li>
         </ul>
       </div>
-      
+      <NotificationContainer />
     </header>
   );
 };
