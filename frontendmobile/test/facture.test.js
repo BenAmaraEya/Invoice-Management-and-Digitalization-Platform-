677d875acfeab23ed2d2ae1d91d3dbd93ec41d65
wafa hamad <hamedw594@gmail.com>
2024-04-30 18:11:53 +0200

    test ajout

--- frontendmobile/test/facture.test.js
+++ frontendmobile/test/facture.test.js
@@ -1,7 +1,16 @@
 import { deleteFacture } from '../pages/delete';
+//import { handleSubmit } from '../pages/formulaire';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { Alert } from 'react-native';
+
+
+
+jest.mock('@react-native-community/datetimepicker', () => {
+  const { View } = require('react-native');
+  const MockDateTimePicker = (props) => <View {...props} testID="mockDateTimePicker" />;
+  return MockDateTimePicker;
+});
 
 jest.mock('@react-native-async-storage/async-storage');
 jest.mock('axios');
@@ -47,3 +56,60 @@ describe('deleteFacture function', () => {
   });
 
 });
+/*describe('handleSubmit function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should add a facture successfully', async () => {
+    AsyncStorage.getItem.mockResolvedValue('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJ1c2VyUHJvZmlsIjoiZm91cm5pc3NldXIiLCJpYXQiOjE3MTQ0NjczMzV9.lqbvUwkHhl7P_LmyiCVuIGzymqL7OKoK4cknlK_qvyY');
+    axios.post.mockResolvedValue({ data: { success: true, message: 'Facture added successfully' } });
+
+    // Suppose you have a function to add a facture that takes the required data as arguments
+    await handleSubmit('123456', 'Facture test', '2024-04-30', '1000', 'USD', 'Nature test', 'AUTRE', '2024-04-30', 'uploads/exemplefa.pdf');
+
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('accessToken');
+    expect(axios.post).toHaveBeenCalledWith('http://192.168.0.5:3006/facture/save/4455', {
+      num_fact: '123456',
+      date_fact: '2024-04-30',
+      montant: '1000',
+      factname: 'Facture test',
+      devise: 'USD',
+      nature: 'Nature test',
+      objet: 'AUTRE',
+      datereception: '2024-04-30',
+      pathpdf: 'uploads/exemplefa.pdf',
+    }, {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJ1c2VyUHJvZmlsIjoiZm91cm5pc3NldXIiLCJpYXQiOjE3MTQ0NjczMzV9.lqbvUwkHhl7P_LmyiCVuIGzymqL7OKoK4cknlK_qvyY'
+      },
+    });
+    expect(Alert.alert).toHaveBeenCalledWith('Success', 'Facture added successfully.');
+  });
+
+  it('should show an error message if adding the facture fails', async () => {
+    AsyncStorage.getItem.mockResolvedValue('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJ1c2VyUHJvZmlsIjoiZm91cm5pc3NldXIiLCJpYXQiOjE3MTQ0NjczMzV9.lqbvUwkHhl7P_LmyiCVuIGzymqL7OKoK4cknlK_qvyY');
+    axios.post.mockRejectedValue(new Error('Failed to add facture'));
+
+    // Suppose you have a function to add a facture that takes the required data as arguments
+    await addFacture('123456', 'Facture test', '2024-04-30', '1000', 'USD', 'Nature test', 'AUTRE', '2024-04-30', 'uploads/exemplefa.pdf');
+
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('accessToken');
+    expect(axios.post).toHaveBeenCalledWith('http://192.168.0.5:3006/facture/save/4455', {
+      num_fact: '123456',
+      date_fact: '2024-04-30',
+      montant: '1000',
+      factname: 'Facture test',
+      devise: 'USD',
+      nature: 'Nature test',
+      objet: 'AUTRE',
+      datereception: '2024-04-30',
+      pathpdf: 'uploads/exemplefa.pdf',
+    }, {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJ1c2VyUHJvZmlsIjoiZm91cm5pc3NldXIiLCJpYXQiOjE3MTQ0NjczMzV9.lqbvUwkHhl7P_LmyiCVuIGzymqL7OKoK4cknlK_qvyY'
+      },
+    });
+    expect(Alert.alert).toHaveBeenCalledWith('Error', 'An error occurred while adding facture: Failed to add facture.');
+  });
+});*/
